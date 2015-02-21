a594a88dd9a2f7df0f6fc1cb1a1e083c093b5f6d
Hamzab <Belainine.hamza@Gmail.com>
2015-02-21 07:46:29 -0500

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -6,10 +6,4 @@ $db = mysql_connect('localhost', 'root', '');
 // on sélectionne la base 
 mysql_select_db('data_tp1', $db);
 
-
-//// on se connecte a MySQL 
-//$db = mysql_connect('zeta2.labunix.uqam.ca', 'jk691240' , 'npdjhGRq'); 
-// 
-//// on sélectionne la base 
-//mysql_select_db('bd_jk691240',$db); 
 ?>
